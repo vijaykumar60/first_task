b4e7988c5713775f26e503d738273111fa906c7e
vijaykumar60 <vijay.kumar@credavenue.com>
2022-02-19 16:45:47 +0530

    Using Tree

--- app/controllers/company_relations_controller.rb
+++ app/controllers/company_relations_controller.rb
@@ -1,12 +1,15 @@
 class CompanyRelationsController < ApplicationController
 
+  include TreeModelConcern
   include CompanyRelationsConcern
 
   def show
-    response = convert_in_trees
+    response = convert_in_trees(JSON_TO_COLUMN_HEADER_MAPPING)
     msg = {:status => "ok", :data => response}
-    render :json => msg
+    render :json => response
+  end
+  
+  def company_rating
 
   end
-    
 end
---
+++ app/models/tree_model.rb
@@ -0,0 +1,7 @@
+class TreeModel
+    attr_accessor
+    def initialize(type)
+        @type = type
+        @data = 
+    end
+end
